1d44dd5e0945597e7dfdea24b5f817aecd22b5ed
awper <awperdev@gmail.com>
2018-11-21 22:04:34 +0100

    added packages

--- src/main/java/org/pioneer/Pioneer.java
+++ src/main/java/org/pioneer/Pioneer.java
@@ -1,4 +1,15 @@
 package org.pioneer;
 
+import org.pioneer.network.PioneerServer;
+
 public class Pioneer {
+    private static PioneerServer server;
+
+    public static void main(String[] args){
+        startServer();
+    }
+
+    private static void startServer(){
+        server = new PioneerServer();
+    }
 }
--- src/main/java/org/pioneer/network/PioneerServer.java
+++ src/main/java/org/pioneer/network/PioneerServer.java
@@ -1,4 +1,10 @@
 package org.pioneer.network;
 
 public class PioneerServer {
+
+    public PioneerServer(){
+
+
+    }
+
 }
